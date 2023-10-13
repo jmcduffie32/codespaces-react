e2c6b03833d13bfdc9f96c3601a7575bea4c7f22
Jon McDuffie <bobber321988@gmail.com>
2023-10-13 00:00:28 +0000

    Get it working on mobile

--- dub-runner/src/components/Match.tsx
+++ dub-runner/src/components/Match.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import _ from "lodash";
 
 interface player {
   id: string;
@@ -10,7 +11,7 @@ interface player {
   team: number;
 }
 
-function shuffle(array: number[]) {
+function shuffle(array: any[]) {
   let currentIndex = array.length, randomIndex;
 
   // While there remain elements to shuffle.
@@ -28,10 +29,11 @@ function shuffle(array: number[]) {
   return array;
 }
 
-const ODD_DOG = -10 // -10 is an arbitrary val for oddDog
+const ODD_DOG = Infinity
 
 const Match = () => {
   const [ players, setPlayers ] = useState<player[]>([]);
+  const [ teams, setTeams ] = useState<player[][]>([]);
 
 
   const [ name, setname ] = useState("");
@@ -40,8 +42,17 @@ const Match = () => {
   const [ bounty, setBounty ] = useState(false);
   const [ oddDog, setOddDog ] = useState(false);
 
+  function reset() {
+    setname("");
+    setCtp(false);
+    setAce(false);
+    setBounty(false);
+    setOddDog(false);
+  }
+
   function addPlayer(name: string, ctp: boolean, ace: boolean, bounty: boolean, oddDog: boolean): void {
     setPlayers((players) => [ ...players, { id: crypto.randomUUID(), name, ctp, ace, bounty, oddDog, team: -1 } ])
+    reset();
   }
 
   function updatePlayer(id: string, patch: Partial<player>): void {
@@ -71,7 +82,7 @@ const Match = () => {
     }
     const shuffled = shuffle(numbers);
 
-    const wantPartners = players.filter((p) => !p.oddDog);
+    const wantPartners = shuffle(players.filter((p) => !p.oddDog));
 
     for (let i = 0; i < wantPartners.length; i++) {
       wantPartners[ i ].team = shuffled[ i ] || ODD_DOG;
@@ -93,17 +104,21 @@ const Match = () => {
 
     const sortedPlayers = wantPartners.concat(wantOddDog).sort((a, b) => b.team - a.team)
 
-    setPlayers(sortedPlayers);
+    const teams = _.map(_.groupBy(sortedPlayers, (p) => p.team), (players, _team) => players)
+
+    setTeams(teams);
   }
 
   return (
     <div className="flex flex-col h-screen">
+      <h1>DUBS</h1>
+
       <div className="mx-auto">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mg-4">
-          <div className="flex md:items-center">
-            <div className="md:flex md:items-center mr-4">
+        <form onSubmit={(e) => e.preventDefault()} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mg-4">
+          <div className="md:flex md:items-center">
+            <div className="mb-4 md:flex md:items-center md:mr-4">
               <div className="md:w-1/3">
-                <label className="block text-gray-700 text-sm font-bold mb-2"
+                <label className="block text-left text-gray-700 text-sm font-bold mb-2"
                   htmlFor="name">
                   Name
                 </label>
@@ -118,50 +133,48 @@ const Match = () => {
             </div>
 
 
-            <div className="md:flex md:items-start mx-4">
-              <label htmlFor="" className="block text-gray-500 font-bold">
+            <div className="mb-2 md:flex md:items-start md:mx-4">
+              <label htmlFor="" className="block text-left text-gray-500 font-bold">
                 <input
                   checked={ctp}
                   onChange={(e) => setCtp(e.target.checked)} type="checkbox" className="mr-2 leading-tight" />
-                <span className="text-gray-800">CTP?</span>
+                <span className="text-gray-800">CTP</span>
               </label>
             </div>
 
-            <div className="md:flex md:items-start mx-4">
-              <label htmlFor="" className="block text-gray-500 font-bold">
+            <div className="mb-2 md:flex md:items-start md:mx-4">
+              <label htmlFor="" className="block text-left text-gray-500 font-bold">
                 <input
                   checked={ace}
                   onChange={(e) => setAce(e.target.checked)} type="checkbox" className="mr-2 leading-tight" />
-                <span className="text-gray-800">ACE?</span>
+                <span className="text-gray-800">Ace</span>
               </label>
             </div>
 
-            <div className="md:flex md:items-start mx-4">
-              <label htmlFor="" className="block text-gray-500 font-bold">
+            <div className="mb-2 md:flex md:items-start md:mx-4">
+              <label htmlFor="" className="block text-left text-gray-500 font-bold">
                 <input
                   checked={bounty}
                   onChange={(e) => setBounty(e.target.checked)} type="checkbox" className="mr-2 leading-tight" />
-                <span className="text-gray-800">BOUNTY?</span>
+                <span className="text-gray-800">Bounty</span>
               </label>
             </div>
 
-            <div className="md:flex md:items-start mx-4">
-              <label htmlFor="" className="block text-gray-500 font-bold">
+            <div className="mb-4 md:flex md:items-start md:mx-4">
+              <label htmlFor="" className="block text-left text-gray-500 font-bold">
                 <input
                   checked={oddDog}
                   onChange={(e) => setOddDog(e.target.checked)} type="checkbox" className="mr-2 leading-tight" />
-                <span className="text-gray-800">ODD DOG?</span>
+                <span className="text-gray-800">Chance to be Odd Dog?</span>
               </label>
             </div>
 
-            <div className="block ml-4">
-              <button
-                type="button"
-                onClick={() => {
-                  addPlayer(name, ctp, ace, bounty, oddDog)
-                }}
-                className="bg-blue-500 text-white py-1 px-4 rounded">ADD</button>
-            </div>
+            <button
+              type="button"
+              onClick={() => {
+                addPlayer(name, ctp, ace, bounty, oddDog)
+              }}
+              className="bg-blue-500 text-white w-full py-1 md:px-4 rounded">ADD</button>
           </div>
 
 
@@ -169,61 +182,76 @@ const Match = () => {
       </div>
 
 
-      <div className="block mt-4 mx-auto">
+
+      <h2 className="my-2">Registration List</h2>
+
+      <table className="table-auto">
+        <thead>
+          <th>Name</th>
+          <th>CTP</th>
+          <th>ACE</th>
+          <th>BOUNTY</th>
+          <th>ODD</th>
+        </thead>
+        {[ ...players ]
+          .sort((a, b) => a.team - b.team)
+          .map(p =>
+            <tr key={p.id}>
+              <td>{p.name}</td>
+              <td className="md:flex md:items-start mx-4">
+                <input
+                  checked={p.ctp}
+                  onChange={(e) => updatePlayer(p.id, { ctp: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
+              </td>
+              <td className="md:flex md:items-start mx-4">
+                <input
+                  checked={p.ace}
+                  onChange={(e) => updatePlayer(p.id, { ace: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
+              </td>
+              <td className="md:flex md:items-start mx-4">
+                <input
+                  checked={p.bounty}
+                  onChange={(e) => updatePlayer(p.id, { bounty: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
+              </td>
+              <td className="md:flex md:items-start mx-4">
+                <input
+                  checked={p.oddDog}
+                  onChange={(e) => updatePlayer(p.id, { oddDog: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
+              </td>
+
+            </tr>
+          )}
+      </table>
+
+      <div className="block mt-4 md:mx-auto">
         <button
           type="button"
           onClick={() => {
             assignTeams(players);
           }}
-          className="bg-blue-500 text-white py-1 px-4 rounded">ASSIGN TEAMS</button>
+          className="bg-blue-500 text-white w-full py-1 px-4 rounded">ASSIGN TEAMS</button>
       </div>
 
 
-      <div className="flex flex-col items-start">
-        {[ ...players ].sort((a, b) => a.team - b.team)
-          .map(p =>
-            <div
-              className="bg-white shadow-md rounded my-2 py-1 px-4 flex"
-              key={p.id}>
-              <div className="font-bold">Name: {p.name}</div>
-              <div className="md:flex md:items-start mx-4">
-                <label htmlFor="" className="block text-gray-500 font-bold">
-                  <input
-                    checked={p.ctp}
-                    onChange={(e) => updatePlayer(p.id, { ctp: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
-                  <span className="text-gray-800">CTP</span>
-                </label>
-              </div>
-              <div className="md:flex md:items-start mx-4">
-                <label htmlFor="" className="block text-gray-500 font-bold">
-                  <input
-                    checked={p.ace}
-                    onChange={(e) => updatePlayer(p.id, { ace: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
-                  <span className="text-gray-800">ACE</span>
-                </label>
-              </div>
-              <div className="md:flex md:items-start mx-4">
-                <label htmlFor="" className="block text-gray-500 font-bold">
-                  <input
-                    checked={p.bounty}
-                    onChange={(e) => updatePlayer(p.id, { bounty: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
-                  <span className="text-gray-800">BOUNTY</span>
-                </label>
-              </div>
-              <div className="md:flex md:items-start mx-4">
-                <label htmlFor="" className="block text-gray-500 font-bold">
-                  <input
-                    checked={p.oddDog}
-                    onChange={(e) => updatePlayer(p.id, { oddDog: e.target.checked })} type="checkbox" className="mr-2 leading-tight" />
-                  <span className="text-gray-800">oddDog</span>
-                </label>
-              </div>
 
-              <div className='font-bold'>Team: {p.team === -10 ? "Odd Dog" : p.team}</div>
-            </div>
-          )}
-      </div>
 
+      {teams.length > 0 &&
+        <>
+          <h2 className="my-2">Teams</h2>
+          <div className="flex flex-wrap justify-center">
+            {[ ...teams ]
+              .sort((a, b) => a[ 0 ].team - b[ 0 ].team)
+              .map(players =>
+                <div className="w-2/5 shadow rounded m-2 p-2" key={players[ 0 ].team}>
+                  <div className='font-bold'>{players[ 0 ].team === ODD_DOG ? "Odd Dog" : players[ 0 ].team}</div>
+                  <div>{players[ 0 ].name}</div>
+                  <div>{players[ 1 ]?.name || ""}</div>
+                </div>
+              )}
+
+          </div>
+        </>
+      }
 
     </div>
   )
