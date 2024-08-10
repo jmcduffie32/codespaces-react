36a60192d49105c1828731b1c6c11561a3db7324
Jon McDuffie <bobber321988@gmail.com>
2024-08-10 19:19:55 -0400

    Fix grouping logic

--- dub-runner/src/utils/teams.test.ts
+++ dub-runner/src/utils/teams.test.ts
@@ -6,28 +6,28 @@ describe('assignToHoles', () => {
     const teams = ['A', 'B', 'C', 'D']
     const oddDogs = ['1', '2']
     const result = assignToHoles(teams, oddDogs)
-    expect(result).toEqual({'1': ['A', 'B'], '10': ['C', '1'], '3':['D', '2']})
+    expect(result).toEqual({'1': ['A', '1'], '10': ['B', '2'], '3':['C', 'D']})
   })
 
   it('should create 2 threesomes with 2 odd dogs', () => {
     const teams = ['A', 'B', 'C', 'D', 'E', 'F']
     const oddDogs = ['1', '2']
     const result = assignToHoles(teams, oddDogs)
-    expect(result).toEqual({'1': ['A', 'B'], '10': ['C', 'D'], '3':['E', '1'], '8':['F', '2']})
+    expect(result).toEqual({'1': ['A', '1'], '10': ['B', '2'], '3':['C', 'D'], '8':['E', 'F']})
   })
 
   it('should create a single 3 some correctly', () => {
     const teams = ['A', 'B', 'C', 'D', 'E']
     const oddDogs = ['1']
     const result = assignToHoles(teams, oddDogs)
-    expect(result).toEqual({'1': ['A', 'B'], '10': ['C', 'D'], '3':['E', '1']})
+    expect(result).toEqual({'1': ['A', '1'], '10': ['B', 'C'], '3':['D', 'E']})
   })
 
   it('should create a single 5 some correctly', () => {
     const teams = ['A', 'B', 'C', 'D', 'E', 'F']
     const oddDogs = ['1']
     const result = assignToHoles(teams, oddDogs)
-    expect(result).toEqual({'1': ['A', 'B'], '10': ['C', 'D'], '3':['E', 'F', '1']})
+    expect(result).toEqual({'1': ['A', 'B', '1'], '10': ['C', 'D'], '3':['E', 'F']})
   })
 
 });
--- dub-runner/src/utils/teams.ts
+++ dub-runner/src/utils/teams.ts
@@ -18,34 +18,24 @@ export function shuffle(array: any[]) {
 }
 
 export function assignToHoles(teams: any[], oddDogs: any[]) {
-  const teamCount = teams.length;
   const holes = [1, 10, 3, 8, 12, 16, 5, 18, 14, 7, 11, 15, 6, 17, 13, 9, 2, 4]
 
   const groups = []
   const assignments: {[key: string]: any[]} = {};
   if (oddDogs.length === 2) {
-    const foursome_count = Math.floor(teamCount / 2) - 1;
-    for (let i = 0; i < foursome_count; i++) {
-      const foursome = [teams.shift(), teams.shift()];
-      groups.push(foursome);
-    }
     groups.push([teams.shift(), oddDogs[0]]);
     groups.push([teams.shift(), oddDogs[1]]);
-    groups.forEach((group, i) => {
-      assignments[`${holes[i]}`] = group;
-    })
-    
+  } else if ((oddDogs.length + teams.length * 2) % 4 === 3) {
+    groups.push([teams.shift(), oddDogs[0]]);
   } else if (oddDogs.length === 1) {
-    const foursome_count = Math.ceil(teamCount / 2) - 1;
-    for (let i = 0; i < foursome_count; i++) {
-      const foursome = [teams.shift(), teams.shift()];
-      groups.push(foursome);
-    }
-    console.log(teams, oddDogs)
-    groups.push([teams.shift(), teams.shift(), oddDogs[0]].filter(Boolean));
-    groups.forEach((group, i) => {
-      assignments[`${holes[i]}`] = group;
-    })
+    groups.push([teams.shift(), teams.shift(), oddDogs[0]]);
   }
+  while (teams.length > 0) {
+    const foursome = [teams.shift(), teams.shift()];
+    groups.push(foursome);
+  }
+  groups.forEach((group, i) => {
+    assignments[`${holes[i]}`] = group;
+  })
   return assignments;
 }
